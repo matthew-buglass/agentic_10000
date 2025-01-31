2fc8ce0081aa9561886a568520db7d7acbb68832
Matthew Buglass <matthew.buglass@andgosystems.com>
2025-01-31 08:11:04 -0600

    Added a link to a tutorial.

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,7 +2,7 @@ import random
 from time import sleep
 
 from websockets.sync.client import connect
-
+# Read through this tutorial to see if there is a better way https://theredrad.medium.com/designing-a-distributed-system-for-an-online-multiplayer-game-basics-part-1-17c149245bd2
 def hello():
     with connect("ws://localhost:8765") as websocket:
         random_number = random.randint(1, 100)
